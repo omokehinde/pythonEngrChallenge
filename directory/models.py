05a166a0bf089979a7cd1d04221e623a9c1d1e90
omokehinde olamide <omokehinde olamide>
2016-12-14 13:19:12 +0100

    directory db migration

--- directory/models.py
+++ directory/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class NameNEmail(models.Model):
+	name = models.CharField(max_length=250)
+	email = models.EmailField(max_length=150)
+
+	# This method makes the terminal to print out the name and email 
+	# whenever you query the db from the terminal usinb NameNEmail.objects.all()
+	def __str__(self):
+		return self.name + ', ' + self.email
